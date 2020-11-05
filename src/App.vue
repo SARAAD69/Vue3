38b92961489bbd9fc366f3c1e673b5788f226a0b
Artemii Chernega <a.chernega2000@gmail.com>
2020-11-05 18:16:44 +0200

    Sending Http Requests - 161

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,41 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
-    <keep-alive>
-      <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
-// import BadgeList from './components/BadgeList.vue';
-// import UserInfo from './components/UserInfo.vue';
-// import CourseGoals from './components/CourseGoals.vue';
-import ActiveGoals from './components/ActiveGoals.vue';
-import ManageGoals from './components/ManageGoals.vue';
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
 
 export default {
   components: {
-    TheHeader,
-    ActiveGoals,
-    ManageGoals,
+    LearningSurvey,
+    UserExperiences,
   },
-  data() {
-    return {
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
-    },
-  },
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     //...
     configureWebpack: {
       devServer: {
-        host: '127.0.0.1'
+        host: 'localhost'
       }
     }
   };
