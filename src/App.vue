0ac6ecc9c0d7edb8a85464012a72593509017f41
Artemii Chernega <a.chernega2000@gmail.com>
2020-12-27 14:33:17 +0200

    Section 16: Main Project: Find a Coach Web App - 256

--- src/App.vue
+++ src/App.vue
@@ -1,186 +1,58 @@
 <template>
+  <the-header></the-header>
   <router-view v-slot="slotProps">
-    <transition name="fade-button" mode="out-in">
+    <transition name="route" mode="out-in">
       <component :is="slotProps.Component"></component>
     </transition>
   </router-view>
-</template>  
+</template>
 
 <script>
+import TheHeader from './components/layout/TheHeader.vue';
+
 export default {
-  data() {
-    return {
-      animatedBlock: false,
-      dialogIsVisible: false,
-      paraIsVisible: false,
-      usersAreVisible: false,
-      enterInterval: null,
-      leaveInterval: null,
-    };
-  },
-  methods: {
-    enterCancelled(el) {
-      console.log(el);
-      clearInterval(this.enterInterval);
-    },
-    leaveCancelled(el) {
-      console.log(el);
-      clearInterval(this.leaveInterval);
-    },
-    beforeEnter(el) {
-      console.log('beforeEnter');
-      console.log(el);
-      el.style.opacity = 0;
-    },
-    enter(el, done) {
-      console.log('enter');
-      console.log(el);
-      let round = 1;
-      this.enterInterval = setInterval(() => {
-        el.style.opacity = round * 0.01;
-        round++;
-        if (round > 100) {
-          clearInterval(this.enterInterval);
-          done();
-        }
-      }, 20);
-    },
-    afterEnter(el) {
-      console.log('afterEnter');
-      console.log(el);
-    },
-    beforeLeave(el) {
-      console.log('beforeLeave');
-      console.log(el);
-      el.style.opacity = 1;
-    },
-    leave(el, done) {
-      console.log('leave');
-      console.log(el);
-      let round = 1;
-      this.leaveInterval = setInterval(() => {
-        el.style.opacity = 1 - round * 0.01;
-        round++;
-        if (round > 100) {
-          clearInterval(this.leaveInterval);
-          done();
-        }
-      }, 20);
-    },
-    afterLeave(el) {
-      console.log('afterLeave');
-      console.log(el);
-    },
-    showUsers() {
-      this.usersAreVisible = true;
-    },
-    hideUsers() {
-      this.usersAreVisible = false;
-    },
-    animateBlock() {
-      this.animatedBlock = true;
-    },
-    toggleParagraph() {
-      this.paraIsVisible = !this.paraIsVisible;
-    },
-    showDialog() {
-      this.dialogIsVisible = true;
-    },
-    hideDialog() {
-      this.dialogIsVisible = false;
-    },
-  },
-};
+  components: {
+    TheHeader
+  }  
+}
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
 * {
   box-sizing: border-box;
 }
+
 html {
-  font-family: sans-serif;
+  font-family: "Roboto", sans-serif;
 }
+
 body {
   margin: 0;
 }
-button {
-  font: inherit;
-  padding: 0.5rem 2rem;
-  border: 1px solid #810032;
-  border-radius: 30px;
-  background-color: #810032;
-  color: white;
-  cursor: pointer;
-}
-button:hover,
-button:active {
-  background-color: #a80b48;
-  border-color: #a80b48;
-}
-.block {
-  width: 8rem;
-  height: 8rem;
-  background-color: #290033;
-  margin-bottom: 2rem;
-  /* transition: transform 0.3s ease-out; */
-}
-.container {
-  max-width: 40rem;
-  margin: 2rem auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 2rem;
-  border: 2px solid #ccc;
-  border-radius: 12px;
-}
-.animate {
-  /* transform: translateX(-150px); */
-  animation: slide-fade 0.3s ease-out forwards;
-}
-
-.fade-button-enter-from,
-.fade-button-leave-to {
-  opacity: 0;
-}
-
-.fade-button-enter-active {
-  transition: opacity 0.3s ease-out;
-}
-
-.fade-button-leave-active {
-  transition: opacity 0.3s ease-in;
-}
-
-.fade-button-enter-to,
-.fade-button-leave-from {
-  opacity: 1;
-}
 
 .route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
 }
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
 .route-enter-active {
-  animation: slide-scale 0.4s ease-out;
-}
-.route-enter-to {
+  transition: all 0.3s ease-out;
 }
 
 .route-leave-active {
-  animation: slide-scale 0.4s ease-in;
+  transition: all 0.3s ease-in;
 }
 
-@keyframes slide-scale {
-  0% {
-    transform: translateX(0) scale(1);
-  }
-
-  70% {
-    transform: translateX(-120px) scale(1.1);
-  }
-
-  100% {
-    transform: translateX(-150px) scale(1);
-  }
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
+import router from './router.js';
+import store from './store/index.js';
 import App from './App.vue';
-import BaseModal from './components/BaseModal.vue';
-import AllUsers from './pages/AllUsers.vue';
-import CourseGoals from './pages/CourseGoals.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: AllUsers },
-    { path: '/goals', component: CourseGoals }
-  ]
-});
-
-const app = createApp(App);
-
-app.component('base-modal', BaseModal);
+const app = createApp(App)
 
 app.use(router);
+app.use(store);
 
-router.isReady().then(function() {
-  app.mount('#app');
-});
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
 
-
+app.mount('#app');
